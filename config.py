e51973f0253ecd9ab99d53887e72aad06e39fc57
Un-bots <161623523+Un-bots@users.noreply.github.com>
2024-03-08 07:41:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,13 +12,13 @@ class Config(object):
     BOT_TOKEN = getenv("BOT_TOKEN", None)              # get from @BotFather
     DATABASE_URL = getenv("DATABASE_URL", None)        # from https://cloud.mongodb.com/
     HELLBOT_SESSION = getenv("HELLBOT_SESSION", None)  # enter your session string here
-    LOGGER_ID = int(getenv("LOGGER_ID", 0))            # make a channel and get its ID
-    OWNER_ID = getenv("OWNER_ID", "")                  # enter your id here
+    LOGGER_ID = int(getenv("LOGGER_ID", "-1002077082091"))            # make a channel and get its ID
+    OWNER_ID = getenv("OWNER_ID", "6253265083")                  # enter your id here
 
     # optional config variables
-    BLACK_IMG = getenv("BLACK_IMG", "https://telegra.ph/file/2c546060b20dfd7c1ff2d.jpg")        # black image for progress
+    BLACK_IMG = getenv("BLACK_IMG", "https://telegra.ph//file/9b79c4c6da60b8479a160.jpg")        # black image for progress
     BOT_NAME = getenv("BOT_NAME", "\x40\x4d\x75\x73\x69\x63\x5f\x48\x65\x6c\x6c\x42\x6f\x74")   # dont put fancy texts here.
-    BOT_PIC = getenv("BOT_PIC", "https://te.legra.ph/file/5d5642103804ae180e40b.jpg")           # put direct link to image here
+    BOT_PIC = getenv("BOT_PIC", "https://te.legra.ph//file/988729c979a7ac3f70c73.jpg")           # put direct link to image here
     LEADERBOARD_TIME = getenv("LEADERBOARD_TIME", "3:00")   # time in 24hr format for leaderboard broadcast
     LYRICS_API = getenv("LYRICS_API", None)             # from https://docs.genius.com/
     MAX_FAVORITES = int(getenv("MAX_FAVORITES", 30))    # max number of favorite tracks
